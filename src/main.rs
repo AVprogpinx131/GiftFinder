f425bb3db2071a8bf7d2e1b9d27fe646bf3c4bab
LENOVO <andruv@taltech.ee>
2024-06-19 11:34:12 +0300

    creating person object, calling vectors function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+mod vectors;
+mod person;
 
-mod cli_game;
+use crate::person::Person;
+use crate::person::Gender;
 
 fn main() {
-    let salary = cli_game::choose_calendar_date().unwrap();
-    
-    cli_game::choose_birthday_item(salary);
+    vectors::vectors_operations();
+
+    let mut person = Person::new (
+        String::from("Andrus Vaher"),
+        20,
+        Gender::Male,
+        vec![String::from("Programming"), String::from("Reading"), String::from("Gaming")],
+    );
+
+    println!("Person: {:?}", person);
+
+    person.set_age(21);
+    person.add_hobby(String::from("Cycling"));
+    person.remove_hobby("Gaming");
+
+    println!("Updated Person: {:?}", person);
+    println!("Number of Hobbies: {}", person.hobby_count());
 }
